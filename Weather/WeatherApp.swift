2757bb1392476af04e8e833a5aa5eb50ad3d89b7
Raul E. Paneque <49737084+rlraul12@users.noreply.github.com>
2022-11-09 09:48:01 -0500

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Raul Paneque on 11/9/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
